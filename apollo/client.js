0e913d2dd22ec79a48036c729bc14c7e10d7a2f4
Arthur <arthur@maxiru.com>
2019-12-15 22:27:13 +0300

    check window

--- apollo/client.js
+++ apollo/client.js
@@ -135,13 +135,15 @@ function createApolloClient(initialState = {}) {
 }
 
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('token')
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : ''
+  if (typeof window !== 'undefined') {
+    // get the authentication token from local storage if it exists
+    const token = localStorage.getItem('token')
+    // return the headers to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : ''
+      }
     }
   }
 })
